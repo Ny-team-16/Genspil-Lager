014fda1a07c63ca936cd69446f5bbecf8ab612e2
Serhat <seab63234@edu.ucl.dk>
2024-04-07 23:52:20 +0200

    Tilføjet ny klasse der kan gemme og udskrive lagerlister

---
+++ Genspil Lager/UdskrivLagerliste.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+public class LagerstatusFilManager
+{
+    public void GemLagerstatusTilFil(List<Lagerstatus> lagerstatusListe, string filnavn)
+    {
+        using (StreamWriter writer = new StreamWriter(filnavn))
+        {
+            foreach (var spil in lagerstatusListe)
+            {
+                writer.WriteLine($"Spil: {spil.Spilnavn}, Version: {spil.Version}, Stand: {spil.Stand}, Pris: {spil.Pris}, Antal: {spil.Antal}");
+            }
+        }
+        Console.WriteLine($"Lagerstatus gemt til fil: {filnavn}");
+    }
+}
